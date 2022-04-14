bbf67488adb1caeb17e931032feda65cc977fcb8
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-04-14 11:28:26 +0900

    Add files via upload

---
+++ Source/Map/FPSProjectGameMode.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSProjectGameMode.h"
+
+FPSProjectGameMode::FPSProjectGameMode()
+{
+}
+
+FPSProjectGameMode::~FPSProjectGameMode()
+{
+}
