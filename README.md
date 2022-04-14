5c0617abf3145ea341daf3c1d1d27d5947791d48
PersesTitan <torampersestitan@gmail.com>
2022-04-14 11:39:00 +0900

    makeMap

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # VR_Project
 vr 맵 제작 및 구현
 
-유니티를 이용한 3d 맵 제작 및 구현, Relativty를 이용한 하드웨어 제작
-
-MPU-6050를 사용함
+Make PersesTitan
---
+++ Source/Map/Map.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Map.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Map, "Map" );
