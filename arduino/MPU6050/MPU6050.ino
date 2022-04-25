9de2693e0b646bf6369f0cc475e2579ca26ad57f
PersesTitan <torampersestitan@gmail.com>
2022-04-26 01:20:16 +0900

    ds

--- arduino/MPU6050/MPU6050.ino
+++ arduino/MPU6050/MPU6050.ino
@@ -1,9 +1,14 @@
+#include "MPU6050.h"
+
 MPU6050 mpu6050(Wire);
 long timer = 0;
 
 void setup() {
   
-
+  Serial.begin(115200);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
 }
 
 void loop() {
